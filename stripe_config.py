6021fa48044aeeb8efda16a8a8b0ce85450a044d
AIASSIST25 <dp.costallc@gmail.com>
2025-07-21 22:24:53 -0400

    Add files via upload

---
+++ stripe_config.py
@@ -0,0 +1,5 @@
+
+# Stripe test keys (replace with your real ones later)
+STRIPE_SECRET_KEY = "sk_test_51MockKeyYourTestSecret"
+STRIPE_PUBLISHABLE_KEY = "pk_test_51MockKeyYourTestPublic"
+STRIPE_PRICE_ID = "price_1MockMonthlyPlan"
